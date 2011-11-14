816da082c74f5891a004275794679889a26a8873
sdrib <doublewizard@gmail.com>
2011-11-14 18:04:31 +0100

    Upadate Header
    Jaja

--- header.php
+++ header.php
@@ -1,1 +1,10 @@
-<div>PROJECT K MEGA HEADER</div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>PROJECT K MEGA HEADER</title>
+        <link rel="stylesheet" type="text/css" href="./assets/css/style.css"/>
+</head>
+
+<body>
+
